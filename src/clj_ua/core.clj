1880707cbfbbaae34b0049e2fb53a2bf1dc229c0
Christopher Coco <cacoco@gmail.com>
2011-12-08 17:02:22 -0800

    Updated with fixes to parse more loosely

--- src/clj_ua/core.clj
+++ src/clj_ua/core.clj
@@ -24,19 +24,34 @@
 
 (defn set-os [#^String part]
   (dosync
-    (let [version (re-find #"([0-9]+)[_.]([A-Za-z0-9]+)" part)]
+    (let [version (re-find #"([0-9]+)[[_.]([A-Za-z0-9]+)]+" part)]
       (if (nil? version)
         ; then
-        (let [second-guess (re-find #"OS X" (.toUpperCase part))]
-          (if (not (nil? second-guess))
-            (alter parsed-platform assoc :name part :version second-guess)))
+        (let [os-x (re-find #"OS X" (.toUpperCase part))]
+          (if (not (nil? os-x))
+            (alter parsed-platform assoc :name part :version os-x)))
         ; else
         (alter parsed-platform assoc :name part :version (.replaceAll (nth version 0) "_" "."))))))
 
 (defn handle-comment [parts callback]
   (if (< 1 (count parts))
     (let [candidate (nth parts 2)]
-      (callback candidate))))
+      (callback candidate))
+    (callback (nth parts 0))))
+
+(defn get-version [#^String s #^String token]
+  (if (< 0 (.indexOf s token))
+    ; then
+    (.substring s (+ (.indexOf s token) 1))
+    ; else
+    (eval s)))
+
+(defn get-name [#^String s #^String token]
+  (if (< 0 (.indexOf s token))
+    ; then
+    (.substring s 0 (.indexOf s token))
+    ; else
+    (eval s)))
 
 (defn parse-comment [#^String seq #^String comment callback]
   (let [is-mozilla (.startsWith (nth (nth seq 0) 0) "Mozilla")
@@ -44,13 +59,13 @@
     (if (and is-mozilla (.equals "compatible" (nth parts 0)))
       ; then
       (let [real-browser (nth parts 1)]
-        (if (< 0 (.indexOf "/" real-browser))
+        (if (< 0 (.indexOf real-browser "/"))
           ; then we have product/version
           (dosync
-            (alter parsed-browser assoc :name (.substring real-browser 0 (.indexOf real-browser "/")) :version (.substring real-browser (+ (.indexOf real-browser "/") 1))))
+            (alter parsed-browser assoc :name (get-name real-browser "/") :version (get-version real-browser "/")))
           ; else we'll have to decipher text
           (dosync
-            (alter parsed-browser assoc :name (.substring real-browser 0 (.indexOf real-browser " ")) :version (.substring real-browser (+ (.indexOf real-browser " ") 1))))))
+            (alter parsed-browser assoc :name (get-name real-browser " ") :version (get-version real-browser " ")))))
       ; else
       (handle-comment parts callback))))
 
@@ -62,7 +77,7 @@
         (if (< 1 (count part))
           ; then
           (let [comment (nth part 6)]
-            (if (and (not (nil? comment)) (nil? (:name @parsed-platform)))
+            (if (and (not (nil? comment)) (.equals (:name @parsed-platform) "unknown"))
               (parse-comment seq comment (fn [candidate] (set-os candidate)))))
           ; else
           (fn []
@@ -73,8 +88,8 @@
 (defn parse [#^String agent]
   (log {:fn "parse" :agent agent})
   (dosync
-    (alter parsed-browser dissoc :name :version)
-    (alter parsed-platform dissoc :name :version))
+    (alter parsed-browser assoc :name "unknown" :version "unknown")
+    (alter parsed-platform assoc :name "unknown" :version "unknown"))
   (let [seq (re-seq #"([^/\s]*)(/([^\s]*))?(\s*\[[a-zA-Z][a-zA-Z]\])?\s*(\((([^()]|(\([^()]*\)))*)\))?\s*" agent)]
     (do-parse seq)
     (let [browser (Browser. (:name @parsed-browser) (:version @parsed-browser))
